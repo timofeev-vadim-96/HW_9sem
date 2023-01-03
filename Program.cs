a4fc8a29fd766ffe5de54ca3617507b3c7ae3c00
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-01-03 21:56:10 +0300

    start 66

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿// Hi there! Switch branches to see homework tasks
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от M до N.
+System.Console.WriteLine("введите значение переменной M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите значение переменной N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string numbers (int A, int B)
+{
+    if (A==B) return B.ToString();
+    return (A + ", " + numbers(A+1, B));
+}
+System.Console.WriteLine(numbers(M, N)); 
