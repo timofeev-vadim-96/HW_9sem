8b2ce5919f1ac8164413f0d2ce5c788904ae7947
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-01-03 21:51:35 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от M до N.
+System.Console.WriteLine("введите значение переменной M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите значение переменной N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string numbers (int A, int B)
+{
+    if (A==B) return B.ToString();
+    return (A + ", " + numbers(A+1, B));
+}
+System.Console.WriteLine(numbers(M, N)); 
