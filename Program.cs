573dd26a76f82050840724a3a08a729858be87b4
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-01-03 22:01:17 +0300

    task 66

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+﻿// Задайте значение значения М и N. Напишите программу, которая найдет сумму натуральных элементов 
 // в промежутке от M до N.
+// M = 4, N = 8 -> 30
 System.Console.WriteLine("введите значение переменной M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("введите значение переменной N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-string numbers (int A, int B)
+int Sum (int A, int B)
 {
-    if (A==B) return B.ToString();
-    return (A + ", " + numbers(A+1, B));
+    if (A==B) return B;
+    return (A + Sum(A+1, B));
 }
-System.Console.WriteLine(numbers(M, N)); 
+System.Console.WriteLine(Sum(M, N)); 
