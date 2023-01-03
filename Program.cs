96225582c19372f6f54c2d08460b26396ecc2087
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-01-03 23:19:37 +0300

    Функция Аккермана

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿// Задайте значение значения М и N. Напишите программу, которая найдет сумму натуральных элементов 
-// в промежутке от M до N.
-// M = 4, N = 8 -> 30
+﻿//Задача 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n
+// m = 3, n = 2 -> A(m,n) = 29 - 
+
+// p.s. Согласно определению Петер
+// А(0,n) = n + 1
+// A(m + 1, 0) = A(m, 1)
+// A(m+1, n+1) = A(m, A(m+1,n))
+
 System.Console.WriteLine("введите значение переменной M: ");
-int M = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("введите значение переменной N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int Sum (int A, int B)
+int n = Convert.ToInt32(Console.ReadLine());
+if (m<0 || n<0) return; //Чтобы соблюсти условие
+
+int Ackerman (int m, int n)
 {
-    if (A==B) return B;
-    return (A + Sum(A+1, B));
+    if (m==0) return n+1;
+    else if (m>0 && n==0) return Ackerman(m-1,1);
+    else return Ackerman(m-1, Ackerman(m, n-1));
 }
-System.Console.WriteLine(Sum(M, N)); 
+System.Console.WriteLine(Ackerman(m, n)); 
